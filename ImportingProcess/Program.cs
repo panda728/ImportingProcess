73114e88da14e4b8554ae815c667864df3617507
HideyukiKitao <panda728z@gmail.com>
2022-07-01 21:49:16 +0900

    Memory<byte>

--- ImportingProcess/Program.cs
+++ ImportingProcess/Program.cs
@@ -4,10 +4,12 @@ using ImportingProcess;
 using System.Text;
 
 #if DEBUG
-var test = new Import01();
-test.FirstVersion();
+//var test = new Import01();
+var test = new Import02();
+await test.FirstVersionAsync();
 Console.WriteLine("Press any key...");
 Console.ReadLine();
 #else
-var summary = BenchmarkRunner.Run<Import01>();
+var summary1 = BenchmarkRunner.Run<Import01>();
+var summary2 = BenchmarkRunner.Run<Import02>();
 #endif
