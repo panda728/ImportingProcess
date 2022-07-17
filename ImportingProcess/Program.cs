57e7fa7f85069fad8c2b3595fb5b1fd168e052e4
HideyukiKitao <panda728z@gmail.com>
2022-07-17 22:45:13 +0900

    不要コード削除

--- ImportingProcess/Program.cs
+++ ImportingProcess/Program.cs
@@ -4,15 +4,10 @@ using ImportingProcess;
 using System.Text;
 
 #if DEBUG
-//var test = new Import01();
-var test = new Import02();
-//await test.ReadStream();
-//await test.Pipeline_SeqPositionAsync();
-//await test.PipeLine_SeqReaderAsync();
+var test = new PipeTest();
 await test.ReadStreamStruct();
 Console.WriteLine("Press any key...");
 Console.ReadLine();
 #else
-//var summary1 = BenchmarkRunner.Run<Import01>();
-var summary2 = BenchmarkRunner.Run<Import02>();
+var summary = BenchmarkRunner.Run<PipeTest>();
 #endif
